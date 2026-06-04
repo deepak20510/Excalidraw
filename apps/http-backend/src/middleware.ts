ae879802562f9a20baef44fc7f224975fd62b960
Deepak Mahato <deepakmahato20510@gmail.com>
2026-06-04 13:19:48 +0530

    added database

--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 
 export function middleware(req: Request, res: Response, next: NextFunction) {
@@ -8,7 +8,7 @@ export function middleware(req: Request, res: Response, next: NextFunction) {
   const decoded = jwt.verify(token, JWT_SECRET);
 
   if (decoded) {
-    req.userId = decoded.userId;
+    (req as any).userId = (decoded as JwtPayload).userId;
     next();
   } else {
     res.status(403).json({
---
+++ packages/db/src/index.ts
@@ -0,0 +1,3 @@
+import { PrismaClient as PrismaClientClass } from "./generated/prisma";
+
+export const PrismaClient = new PrismaClientClass();
